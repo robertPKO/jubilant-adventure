e008bf8b9d11a26f3fa80a6f63783561c9947cf9
Robert Tran <roberttran@ad.pakko.com.au>
2023-12-21 15:48:16 +1000

    first commit

--- app/page.js
+++ app/page.js
@@ -1,25 +1,4 @@
-"use client";
-import { Suspense } from "react";
-import { RoomModel } from "../components/models/RoomModel";
-import { Canvas } from "@react-three/fiber";
-
+import ModelSection from "../components/models/ModelSection";
 export default function Home() {
-
-
-  return (
-    <div className="w-full h-[100vh]">
-      <Canvas>
-        <ambientLight intensity={1.5} />
-        <directionalLight
-          castShadow
-          position={[5,5,5]}
-          shadow-mapSize-width={2048} // Increasing shadow map width
-          shadow-mapSize-height={2048} // Increasing shadow map height
-        />{" "}
-        <Suspense>
-          <RoomModel />
-        </Suspense>
-      </Canvas>
-    </div>
-  );
+  return <ModelSection />;
 }
---
+++ components/models/ModelSection.js
@@ -0,0 +1,23 @@
+"use client";
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { RoomModel } from "./RoomModel";
+
+export default function ModelSection() {
+  return (
+    <div className="w-full h-[100vh]">
+      <Canvas>
+        <ambientLight intensity={1.5} />
+        <directionalLight
+          castShadow
+          position={[5, 5, 5]}
+          shadow-mapSize-width={2048} // Increasing shadow map width
+          shadow-mapSize-height={2048} // Increasing shadow map height
+        />{" "}
+        <Suspense>
+          <RoomModel />
+        </Suspense>
+      </Canvas>
+    </div>
+  );
+}
