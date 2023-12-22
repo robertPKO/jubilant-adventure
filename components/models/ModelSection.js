02fa93cc4e9a74ffe0b534c54ac41bb573465494
Robert Tran <roberttran@ad.pakko.com.au>
2023-12-22 10:34:18 +1000

    first

--- components/models/ModelSection.js
+++ components/models/ModelSection.js
@@ -11,20 +11,22 @@ const ui = tunnel();
 export default function ModelSection() {
   return (
     <div className="w-full h-[100vh]">
-      <ui.In>
-        {" "}
-        <ambientLight intensity={1.5} />
-        <directionalLight
-          castShadow
-          position={[5, 5, 5]}
-          shadow-mapSize-width={2048} // Increasing shadow map width
-          shadow-mapSize-height={2048} // Increasing shadow map height
-        />{" "}
-        <Suspense>
-          <RoomModel ui={ui} />
-        </Suspense>
-        <Preload all />{" "}
-      </ui.In>
+      <div>
+        <ui.In>
+          {" "}
+          <ambientLight intensity={1.5} />
+          <directionalLight
+            castShadow
+            position={[5, 5, 5]}
+            shadow-mapSize-width={2048} // Increasing shadow map width
+            shadow-mapSize-height={2048} // Increasing shadow map height
+          />{" "}
+          <Suspense>
+            <RoomModel ui={ui} />
+          </Suspense>
+          <Preload all />{" "}
+        </ui.In>
+      </div>
       <Canvas>
         <ui.Out />
       </Canvas>
